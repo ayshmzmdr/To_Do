88007a37d662de8acd9119e513084c2359c38f4a
Ayush Mazumdar <ayush.mazumdar111@gmail.com>
2024-06-03 22:14:02 +0530

    Created the frontend for the application

--- main/forms.py
+++ main/forms.py
@@ -2,15 +2,15 @@ from django import forms
 
 class TodoForm(forms.Form):
     name = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'To-do name here'}),
+        widget=forms.TextInput(attrs={'placeholder': 'To-do name here','style':'width: 350px;height:50px;'}),
         max_length=150,
         label=""
     )
 
 class TaskForm(forms.Form):
     name_task = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Your Task here'}),
+        widget=forms.TextInput(attrs={'placeholder': 'Your Task here',"style":"width: 350px;height:50px;"}),
         max_length=250,
         label="",
-        required=False
+        required=False,
     )
